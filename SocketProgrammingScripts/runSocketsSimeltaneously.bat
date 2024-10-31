225317b5907af5604932728fcf6ab52ef2ebe0f5
Cezar <cezatrongaming@yahoo.com>
2024-10-31 21:23:15 +0000

    Fixed little bug

--- SocketProgrammingScripts/runSocketsSimeltaneously.bat
+++ SocketProgrammingScripts/runSocketsSimeltaneously.bat
@@ -1,8 +1,8 @@
 @echo off
-start cmd /k "cd /d C:\gitProjects\Socket programming && python socketServer.py" 
+start cmd /k "cd /d C:\gitProjects\Socket programming\SocketProgrammingScripts && python socketServer.py" 
 echo Starting both scripts in 3 seconds...
 
 timeout /t 3 /nobreak >nul
 
-start cmd /k "cd /d C:\gitProjects\Socket programming && python socketClient.py"
-start cmd /k "cd /d C:\gitProjects\Socket programming && python socketClient2.py"
+start cmd /k "cd /d C:\gitProjects\Socket programming\SocketProgrammingScripts && python socketClient.py"
+start cmd /k "cd /d C:\gitProjects\Socket programming\SocketProgrammingScripts && python socketClient2.py"
